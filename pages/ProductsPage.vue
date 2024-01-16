4432988506885468be2cfd26f01ac2b27be66dbf
Mikkel Beck Knudsen <47305516+BeckMK1@users.noreply.github.com>
2024-01-16 16:06:12 +0100

    added a new btn

--- pages/ProductsPage.vue
+++ pages/ProductsPage.vue
@@ -8,11 +8,21 @@
 			<div class="content">
 				<AccordionsAccordionProductCom v-for="(product, index) in store.products" :displayTitle="product.title" :currentSlideId="index"></AccordionsAccordionProductCom>
 			</div>
+			<button class="btn saveBtn" @click="PostToDb">Save to database</button>
 		</div>
 	</div>
 </template>
 <script setup>
 	import { useStore } from "@/store/glStore"
 	const store = useStore()
+	function PostToDb(){
+		$fetch('http://localhost:3002/api/post', {
+			method:'POST',
+			body:{products: store.products}
+		})
+	}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+
+</style>
